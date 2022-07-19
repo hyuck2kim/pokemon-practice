86ff4695da5119d63efbd165f29e3a97b7c3f8bf
hyuck2kim <sang_zz@naver.com>
2022-07-19 18:22:58 +0900

    second day commit: finished pokemon list(index page)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/indexPage";
+import DetailPage from "./pages/DetailPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/:id" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
